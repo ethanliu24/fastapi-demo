e924b739a08d71d99a90bf9826c5f25684632491
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-11 18:23:45 -0500

    implement a wrapper for databases

--- app/util/db.py
+++ app/util/db.py
@@ -1,12 +1,27 @@
-"""
-For dependency injection for database.
-"""
+# For dependency injection for database.
+
 from database.database import Database
 from database.mongodb import MongoDB
-from settings import ENVIORNMENT
+from repository.repository import Repository
+from repository.mongodb import UserRepository as UserRepository
+from settings import ENVIORNMENT, USER_COLLECTION
 
-def get_database() -> Database:
-    # Might use different database or clustors on development, production or testing
-    if ENVIORNMENT == "development":
-        return MongoDB()
-    return MongoDB()
+class DB:
+    """
+    Wrapper class for accessing a database.
+    Might use different database or clustors on development, production or testing.
+    """
+
+    _db: Database
+    _user_repository: Repository
+
+    def __init__(self) -> None:
+        # Might use different database or clustors on development, production or testing.
+        # if ENVIORNMENT == "development":
+        #     pass
+        mongodb = MongoDB()
+        self._db = mongodb
+        self._user_repository = UserRepository(mongodb.get_collection(USER_COLLECTION))
+
+    def get_user_repository(self) -> Repository:
+        return self._user_repository
