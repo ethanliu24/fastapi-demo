5346ee4f7b2fe45ac7dbd7880a1fd0807efbbd94
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-11 18:22:28 -0500

    modify file path imports

--- app/repository/mongodb/mongodb_orm.py
+++ app/repository/mongodb/mongodb_orm.py
@@ -1,6 +1,7 @@
-from repository import Repository
+from ..repository import Repository
 from pymongo.collection import Collection
 
 class MongoDBORM(Repository):
     def __init__(self, repository: Collection):
         super().__init__(repository)
+        
--- app/repository/mongodb/user_repository.py
+++ app/repository/mongodb/user_repository.py
@@ -1,7 +1,37 @@
-from mongodb_orm import MongoDBORM
+from .mongodb_orm import MongoDBORM
 from pymongo.collection import Collection
+from schema.user import User
 
 class UserRepository(MongoDBORM):
     def __init__(self, repository: Collection):
         super().__init__(repository)
-        
+
+    def insert(self, user: User):
+        """
+        Inserts a schema into the given <self._repository>.
+        """
+        pass
+
+    def get(self, filter: dict):
+        """
+        Retrieves a schema from <self._repository> with the applied filter.
+        """
+        pass
+
+    def get_all(self, filter: dict):
+        """
+        Retrieves all schemas from <self._repository> with the applied filter.
+        """
+        pass
+
+    def delete(self, filter: dict):
+        """
+        Deletes a schema from <self._repository> with the applied filter.
+        """
+        pass
+
+    def update(self, filter: dict):
+        """
+        Updates a schema from <self._repository> with the applied filter.
+        """
+        pass
