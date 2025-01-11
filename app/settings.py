b6d93cf74f41ca3d3bc46e28fdbdb86f2734267f
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-11 02:45:24 -0500

    add config file

---
+++ app/settings.py
@@ -0,0 +1,14 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
+
+ENVIORNMENT = os.environ.get("ENVIORNMENT")
+
+MONGO_DB_CONNECTION_STRING = os.environ.get("MONGO_DB_CONNECTION_STRING")
+DEMO_CLUSTER = "DemoCluster"
+USER_COLLECTION = "users"
+
+print(ENVIORNMENT)
