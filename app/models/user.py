9ef7dd46d425b2c40a6ff9ad64b1058f49bcec2f
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-12 02:06:08 -0500

    add user sign up schema

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(BaseModel):
     """
     Represents an application user.
     """
-    _id: Optional[ObjectId]
+    _id: ObjectId
     email: EmailStr
     username: str
     password: str
@@ -15,16 +15,20 @@ class User(BaseModel):
     created_at: datetime
     modified_at: datetime
 
+class StandardUserSignUp(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+    password_confirmation: str
+
     @field_validator("password")
     def validate_password(password: str) -> str:
         if len(password) < 2:
             raise ValueError("Password must be at least 2 characters long.")
-        # TODO hash it
         return password
 
     @field_validator("password_confirmation")
     def confirm_password(confirmation: str, fields: dict) -> str:
-        # TODO edit to hash
         password = fields.data.get("password")
         if confirmation != password:
             raise ValueError("Password doesn't match")
