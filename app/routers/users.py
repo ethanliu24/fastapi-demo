b71f208f4f6d5a1b7a56a870376602aca9c592ba
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-12 00:54:12 -0500

    add user routes boilerplate

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,32 @@
 from fastapi import APIRouter, Depends, HTTPException
+from repositories.repository import Repository
+from ..dependencies import get_user_repository
 
 router = APIRouter(
-    prefix="users",
-    tags=["users"]
+    prefix="/users",
+    tags=["users"],
 )
 
+@router.get("/")
+async def get_users(user_repository: Repository = Depends(get_user_repository)):
+    pass
+
+
+@router.post("/")
+async def create_user(user_repository: Repository = Depends(get_user_repository)):
+    pass
+
+
+@router.get("/{user_id}")
+async def get_user(user_repository: Repository = Depends(get_user_repository)):
+    pass
+
+
+@router.put("/{user_id}")
+async def get_user(user_repository: Repository = Depends(get_user_repository)):
+    pass
+
+
+@router.delete("/{user_id}")
+async def get_user(user_repository: Repository = Depends(get_user_repository)):
+    pass
