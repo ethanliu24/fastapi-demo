fb7c6fdaa074415ae5a2e1253c60f9f1947dc0f3
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-12 00:03:44 -0500

    set up routers for api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from .routers import users
 
 app = FastAPI()
 
+app.include_router(users.router)
+
 @app.get("/")
-def root():
-    return "Hello World"
+async def root():
+    return "Hello World!"
---
+++ app/routers/users.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+router = APIRouter(
+    prefix="users",
+    tags=["users"]
+)
+
