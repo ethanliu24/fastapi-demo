61724fa91a91317a3e7cc8eb9976f85edc7536d1
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-11 23:50:04 -0500

    move dependencies to a new module

--- app/db.py
+++ app/db.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 from database.db_manager import DBManager
 from database.mongodb import MongoDB
-from fastapi import Depends
-from typing import Annotated
 from .repositories.repository import Repository
 from .repositories.mongodb import UserRepository as UserRepository
 from config.settings import (
@@ -12,7 +10,6 @@ from config.settings import (
 
 class DB:
     """
-    This class should not be instantiated outside this file - FastAPI will create a dependency for it.
     Wrapper class for databases. Services in this app will refer to this class to access different
     databases. Might use different database or clustors on development, production or testing.
     """
@@ -38,12 +35,3 @@ class DB:
         user_collection = mongodb.get_collection(USER_COLLECTION)
         user_collection.create_index("email", unique=True)
         self._user_repository = UserRepository(user_collection)
-
-
-# Fast API will automatically inject these due to the dependency injection feature
-def get_db() -> DB:
-    return DB()
-
-
-def get_user_repository(db: Annotated[DB, Depends(get_db)]) -> Repository:
-    return db.get_user_repository()
---
+++ app/dependencies.py
@@ -0,0 +1,13 @@
+# Fast API will automatically inject these as its dependency injection feature
+from .db import DB
+from fastapi import Depends
+from typing import Annotated
+from repositories.repository import Repository
+
+
+def get_db() -> DB:
+    return DB()
+
+
+def get_user_repository(db: Annotated[DB, Depends(get_db)]) -> Repository:
+    return db.get_user_repository()
