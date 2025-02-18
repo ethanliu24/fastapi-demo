00419eab5430de06123ffc3d43eedf26d25aca94
ethanliu24 <liu.ethan2005@gmail.com>
2025-02-17 23:48:31 -0500

    enable cors for all origins to access app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from .config.routes import api_router_v1
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(api_router_v1)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
