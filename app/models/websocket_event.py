ec131524fbe455c33fbd712ab369f8db6ad23741
ethanliu24 <liu.ethan2005@gmail.com>
2025-02-18 11:46:04 -0500

    add models for websocket event types

---
+++ app/models/websocket_event.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class PublicChatEvent(BaseModel):
+    event: str
+    message: str
+
+
+class TypingEvent(BaseModel):
+    event: str
+    user: str
+    is_typing: bool
