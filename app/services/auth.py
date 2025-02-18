380befbe2e36b5676778b2c2bd44e3f17b3cc0a8
ethanliu24 <liu.ethan2005@gmail.com>
2025-02-18 15:29:47 -0500

    add jwt decoder

--- app/services/auth.py
+++ app/services/auth.py
@@ -3,6 +3,7 @@ from ..models.authentication import StandardUserSignUp, JWTToken
 from ..models.user import User
 from datetime import timedelta, datetime, timezone
 import jwt
+from jwt.exceptions import InvalidTokenError
 from ..config.settings import ACCESS_TOKEN_EXPIRE_MINUTES, JWT_SECRET_KEY, JWT_HASH_ALORITHM
 
 class AuthenticationServices:
@@ -19,14 +20,22 @@ class AuthenticationServices:
         encoded_jwt = jwt.encode(encode, JWT_SECRET_KEY, algorithm=JWT_HASH_ALORITHM)
         return encoded_jwt
 
-    async def current_user(self) -> User:
-        pass
+    async def get_current_user(self, token: str) -> User:
+        try:
+            payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_HASH_ALORITHM])
+            user_id = payload.get("sub")
+            if not user_id:
+                return None
+        except InvalidTokenError:
+            return None
+
+        try:
+            return await self._user_services.get_user(user_id)
+        except ValueError:
+            return None
 
     async def signup(self, user_data: StandardUserSignUp) -> JWTToken:
         user = self._user_services.create_user(user_data)
-        token = self.generate_token(data={"sub": user.username})
+        token = self.generate_token(data={"sub": user.id})
         return JWTToken(access_token=token, token_type="bearer")
 
-    async def authenticate_user(self) -> User | None:
-        # user = self._user_services.get_user()
-        pass
